ec2dbedd5e5e67f7646a783c5f0adf914fe961f9
Mais Jamil <mais@Maiss-MacBook-Air.local>
2024-02-24 22:03:02 +0300

    adding .env.example

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const spoonacular = axios.create({
-    baseURL: "https://api.spoonacular.com",
-    headers: {
-        "X-API-KEY": import.meta.env.VITE_API_URL
-    }
-})
+  baseURL: "https://api.spoonacular.com",
+  headers: {
+    "X-API-KEY": import.meta.env.VITE_API_KEY,
+  },
+});
